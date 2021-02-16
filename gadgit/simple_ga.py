5969e5597d903dc5da87f5a8ff41fc9fb723bf94
Tyler Collins <tk11br@sharcnet.ca>
2021-02-16 13:31:33 -0500

    add assertion structure
    the value error structure was awful
    switching to something more professional

--- gadgit/simple_ga.py
+++ gadgit/simple_ga.py
@@ -9,44 +9,30 @@ from deap import base
 from deap import creator
 from deap import tools
 
-# random.seed(64)
-
 data_frame = pd.read_pickle(sys.argv[1])
 
-MAX_ITEM = 100
-NBR_ITEMS = data_frame.shape[0]
+COM_SIZE = 100
+GENE_COUNT = data_frame.shape[0]
 
-global_frontier = [0 for x in range(NBR_ITEMS)]
+FRONTIER = [0 for x in range(GENE_COUNT)]
 
 creator.create("Fitness", base.Fitness, weights=(1.0,))
 creator.create("Individual", set, fitness=creator.Fitness)
 
-def dummy_init(val):
-    return 100
-
 toolbox = base.Toolbox()
-# toolbox.register("attr_item", random.randrange, NBR_ITEMS)
-# toolbox.register("attr_item", dummy_init, NBR_ITEMS)
-# toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_item, MAX_ITEM)
-toolbox.register("indices", random.sample, range(NBR_ITEMS), MAX_ITEM)
+toolbox.register("indices", random.sample, range(GENE_COUNT), COM_SIZE)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 def evalKnapsack(individual):
-    if len(individual) != MAX_ITEM:
-        raise ValueError('Indiv has wrong number of things')
+    assert len(individual) == COM_SIZE, 'Indiv does not match community size in eval'
 
-    between = 0.0
+    fit_sum = 0.0
     for item in individual:
-        # degree += data_frame.loc[item, 'Degree']
-        # between += data_frame.loc[item, 'Betweenness']
-        # degree += data_frame.iloc[item]['Degree']
-        between += data_frame.iloc[item]['Betweenness']
-        global_frontier[item] += 1
-    if len(individual) > MAX_ITEM:
-        return -10000, # -10000
-    # return degree, between
-    return between, 
+        fit_sum += data_frame.iloc[item]['Betweenness']
+        FRONTIER[item] += 1
+    
+    return fit_sum, 
     
 def cxSDB(ind1, ind2):
     """SDB Crossover as defined in thesis."""
@@ -56,8 +42,7 @@ def cxSDB(ind1, ind2):
     dealer = list(ind1.union(ind2) - intersect)
     random.shuffle(dealer)
 
-    if len(dealer) % 2 != 0:
-        raise ValueError('Dealer assumption on indiv crossover failure')
+    assert len(dealer) % 2 == 0, 'Dealer assumption on indiv crossover failure'
 
     # Clear old sets
     ind1.clear()
@@ -69,20 +54,16 @@ def cxSDB(ind1, ind2):
     ind2.update(dealer[len(dealer)//2:])
     ind2.update(intersect)
 
-    if len(ind1) != MAX_ITEM or len(ind2) != MAX_ITEM:
-        raise ValueError('SDB created invalid indiv')
+    assert len(ind1) == COM_SIZE and len(ind2) == COM_SIZE, 'SDB created invalid individual'
     
     return ind1, ind2
 
 def mutFlipper(individual):
-    if len(individual) == MAX_ITEM: # Not sure if empty is truly possible; keeping for now.
-        off_index = random.choice(list(set(range(0, NBR_ITEMS)) - individual))
-        individual.remove(random.choice(sorted(tuple(individual))))
-        individual.add(off_index)
-        if len(individual) != MAX_ITEM:
-            raise ValueError('Mutation created an invalid indiv')
-    else:
-        raise ValueError('Mutation received invalid indiv')
+    assert len(individual) == COM_SIZE, 'Mutation received invalid indiv'
+    off_index = random.choice(list(set(range(0, GENE_COUNT)) - individual))
+    individual.remove(random.choice(sorted(tuple(individual))))
+    individual.add(off_index)
+    assert len(individual) == COM_SIZE, 'Mutation created an invalid indiv'
 
     return individual,
 
@@ -91,7 +72,6 @@ toolbox.register("mate", cxSDB)
 toolbox.register("mutate", mutFlipper)
 
 def main():
-    # random.seed(64)
     NGEN = 100
     NPOP = 25
     CXPB = 0.75
@@ -122,7 +102,7 @@ if __name__ == "__main__":
         buf += data_frame.iloc[ind]['GeneName'] + ', '
     print(buf)
     print('Nodes exloration count: ')
-    print(global_frontier)
-    missed_nodes = [data_frame.iloc[ind]['GeneName'] for ind, x in enumerate(global_frontier) if x == 0]
+    print(FRONTIER)
+    missed_nodes = [data_frame.iloc[ind]['GeneName'] for ind, x in enumerate(FRONTIER) if x == 0]
     print('Nodes never explored (bad): N =', len(missed_nodes))
     print(', '.join(missed_nodes))
