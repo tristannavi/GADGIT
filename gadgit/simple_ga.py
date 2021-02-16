daca189a708c30e3fd67074f0b211b2c3f111fde
Tyler Collins <tk11br@sharcnet.ca>
2021-02-16 12:36:17 -0500

    fix SDB crossover, fix indiv gen

--- gadgit/simple_ga.py
+++ gadgit/simple_ga.py
@@ -13,22 +13,29 @@ from deap import tools
 
 data_frame = pd.read_pickle(sys.argv[1])
 
-IND_INIT_SIZE = 100
 MAX_ITEM = 100
 NBR_ITEMS = data_frame.shape[0]
 
 global_frontier = [0 for x in range(NBR_ITEMS)]
 
-creator.create("Fitness", base.Fitness, weights=(1.0, 1.0))
+creator.create("Fitness", base.Fitness, weights=(1.0,))
 creator.create("Individual", set, fitness=creator.Fitness)
 
+def dummy_init(val):
+    return 100
+
 toolbox = base.Toolbox()
-toolbox.register("attr_item", random.randrange, NBR_ITEMS)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_item, IND_INIT_SIZE)
+# toolbox.register("attr_item", random.randrange, NBR_ITEMS)
+# toolbox.register("attr_item", dummy_init, NBR_ITEMS)
+# toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_item, MAX_ITEM)
+toolbox.register("indices", random.sample, range(NBR_ITEMS), MAX_ITEM)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 def evalKnapsack(individual):
-    degree = 0.0
+    if len(individual) != MAX_ITEM:
+        raise ValueError('Indiv has wrong number of things')
+
     between = 0.0
     for item in individual:
         # degree += data_frame.loc[item, 'Degree']
@@ -40,38 +47,47 @@ def evalKnapsack(individual):
         return -10000, # -10000
     # return degree, between
     return between, 
-
-def cxSet(ind1, ind2):
-    """Apply a crossover operation on input sets. The first child is the
-    intersection of the two sets, the second child is the difference of the
-    two sets.
-    """
-    temp = set(ind1)                # Used in order to keep type
-    ind1 &= ind2                    # Intersection (inplace)
-    ind2 ^= temp                    # Symmetric Difference (inplace)
-    return ind1, ind2
     
-def mutSet(individual):
-    """Mutation that pops or add an element."""
-    if random.random() < 0.5:
-        if len(individual) > 0:     # We cannot pop from an empty set
-            individual.remove(random.choice(sorted(tuple(individual))))
-    else:
-        individual.add(random.randrange(NBR_ITEMS))
-    return individual,
+def cxSDB(ind1, ind2):
+    """SDB Crossover as defined in thesis."""
+    intersect = ind1.intersection(ind2) # New indivs start with intersection
+
+    # Grab the other genes and shuffle them
+    dealer = list(ind1.union(ind2) - intersect)
+    random.shuffle(dealer)
+
+    if len(dealer) % 2 != 0:
+        raise ValueError('Dealer assumption on indiv crossover failure')
+
+    # Clear old sets
+    ind1.clear()
+    ind2.clear()
+
+    # Split list in half and assign to new indivs
+    ind1.update(dealer[:len(dealer)//2])
+    ind1.update(intersect)
+    ind2.update(dealer[len(dealer)//2:])
+    ind2.update(intersect)
+
+    if len(ind1) != MAX_ITEM or len(ind2) != MAX_ITEM:
+        raise ValueError('SDB created invalid indiv')
+    
+    return ind1, ind2
 
 def mutFlipper(individual):
-    """Mutation as specified in thesis."""
-    if len(individual) > 0: # Not sure if empty is truly possible; keeping for now.
+    if len(individual) == MAX_ITEM: # Not sure if empty is truly possible; keeping for now.
         off_index = random.choice(list(set(range(0, NBR_ITEMS)) - individual))
         individual.remove(random.choice(sorted(tuple(individual))))
         individual.add(off_index)
-    else: # Mutation rebuilds if empty.
-        individual.add(random.randrange(NBR_ITEMS))
+        if len(individual) != MAX_ITEM:
+            raise ValueError('Mutation created an invalid indiv')
+    else:
+        raise ValueError('Mutation received invalid indiv')
+
     return individual,
 
 toolbox.register("evaluate", evalKnapsack)
-toolbox.register("mate", cxSet)
+toolbox.register("mate", cxSDB)
 toolbox.register("mutate", mutFlipper)
 
 def main():
