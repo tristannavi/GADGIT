79f118f385176a2233774fc3402cab5cf0ce5bef
Tyler Collins <tk11br@sharcnet.ca>
2021-02-17 14:46:32 -0500

    add mutation fixed genes support

--- gadgit/ga_single.py
+++ gadgit/ga_single.py
@@ -41,6 +41,8 @@ def cxSDB(gene_info, ind1, ind2):
     and lastly hands out half of the shuffled dealer to each indiv.
 
     ind1 and ind2 and kept as objects since they inherit from set, but have additional properties.
+
+    Note that this process is not destructive to the fixed genes inside of the individuals.
     """
 
     # Build dealer
@@ -57,16 +59,22 @@ def cxSDB(gene_info, ind1, ind2):
     ind2.update(dealer[len(dealer)//2:])
     ind2.update(intersect)
     assert len(ind1) == gene_info.com_size and len(ind2) == gene_info.com_size, 'SDB created invalid individual'
-    
+    assert set(gene_info.fixed_list_ids).issubset(ind1), 'Ind1 does not possess all fixed genes after crossover'
+    assert set(gene_info.fixed_list_ids).issubset(ind2), 'Ind2 does not possess all fixed genes after crossover'
+
     return ind1, ind2
 
 def mutFlipper(gene_info, individual):
-    """Flip based mutation. Flip one off to on, and one on to off."""
+    """Flip based mutation. Flip one off to on, and one on to off.
+    
+    Must not allow the choice of a fixed gene to be turned off.
+    """
     assert len(individual) == gene_info.com_size, 'Mutation received invalid indiv'
     off_index = random.choice(list(set(range(0, gene_info.gene_count)) - individual))
-    individual.remove(random.choice(sorted(tuple(individual))))
+    individual.remove(random.choice(sorted(tuple(individual - set(gene_info.fixed_list_ids)))))
     individual.add(off_index)
     assert len(individual) == gene_info.com_size, 'Mutation created an invalid indiv'
+    assert set(gene_info.fixed_list_ids).issubset(individual), 'Individual does not possess all fixed genes after mutation'
 
     return individual,
 
