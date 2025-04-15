ab232bae998aaca34e0e98de3d550a40ff9f8632
Tristan <17625517+tristannavi@users.noreply.github.com>
2025-04-15 15:45:57 -0400

    ?

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Andesha/gadgit',
-    version="8.0.0",
+    version="8.1.0",
     zip_safe=False,
 )
 
