26cb404e50768569b0d1f58332ecc7b17a801b9b
Tristan <17625517+tristannavi@users.noreply.github.com>
2025-04-21 16:29:59 -0400

    ?

--- gadgit/__init__.py
+++ gadgit/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Tyler Collins"""
 __email__ = 'tk11br@sharcnet.ca'
 __version__ = '0.6.0'
+
+import gadgit.GAOutput
+import gadgit.GeneInfo
+import gadgit.GAInfo
+import gadgit.gadgit
+import gadgit.post_run
