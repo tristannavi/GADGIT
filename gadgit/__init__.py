7847d284aa71569f2ee0358edabd0f205da7d50e
Tristan <17625517+tristannavi@users.noreply.github.com>
2025-04-21 16:31:38 -0400

    ?

--- gadgit/__init__.py
+++ gadgit/__init__.py
@@ -1,11 +1,11 @@
 """Top-level package for GADGIT."""
-
 __author__ = """Tyler Collins"""
 __email__ = 'tk11br@sharcnet.ca'
 __version__ = '0.6.0'
 
-import gadgit.GAOutput
-import gadgit.GeneInfo
-import gadgit.GAInfo
-import gadgit.gadgit
-import gadgit.post_run
+from .GAInfo import GAInfo
+from .GAOutput import GAOutput
+from .GeneInfo import GeneInfo
+from .gadgit import ga_multi
+from .gadgit import ga_single
+from .post_run import post_run
