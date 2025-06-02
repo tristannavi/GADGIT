5c8aa4a49cc1980a8737a3f980dfdfbb44b9f18d
Tristan <17625517+tristannavi@users.noreply.github.com>
2025-06-02 19:50:32 -0400

    Revert elite and tournament selection to prioritize maximization instead of minimization

--- gadgit/gadgit.py
+++ gadgit/gadgit.py
@@ -331,15 +331,15 @@ def ea_sum_of_ranks(ga_info: GAInfo, gene_info: GeneInfo, population: NDArray, c
     fit_series: NDArray
     fit_series = multi_eval_nb(gene_info.data_numpy, population, gene_info.sum)
 
-    # elite = [deepcopy(population[fit_series.argmax()])]
-    elite = [deepcopy(population[fit_series.argmin()])]
+    elite = [deepcopy(population[fit_series.argmax()])]
+    # elite = [deepcopy(population[fit_series.argmin()])]
 
     # Begin the generational process
     for gen in range(1, ngen + 1):
         if gen % 10 == 0:
             print(gen)
         # Select the next generation individuals to breed
-        breed_pop = tournament_selection(gene_info, population, len(population) - 1, ga_info.nk, fit_series, max=False)
+        breed_pop = tournament_selection(gene_info, population, len(population) - 1, ga_info.nk, fit_series, max=True)
 
         offspring = varAnd(breed_pop, cxpb, mutpb, gene_info, cross_meth, len(population) - 1)
 
@@ -354,10 +354,11 @@ def ea_sum_of_ranks(ga_info: GAInfo, gene_info: GeneInfo, population: NDArray, c
         # best_offspring_fitness = offspring[fit_series.argmin()].fitness.values[0]
         # elite_fitness = fit_series[offspring.index(elite[0])]
 
-        # best_current = fit_series.argmax()
-        # current_elite_fitness = fit_series[np.where((offspring == elite[0]).all(1))[0][0]]
-        # elite = [deepcopy(offspring[fit_series.argmax()]) if best_current >= current_elite_fitness else elite[0]]
-        elite = [deepcopy(offspring[fit_series.argmin()])]
+        best_current = fit_series.argmax()
+        current_elite_fitness = fit_series[np.where((offspring == elite[0]).all(1))[0][0]]
+        elite = [deepcopy(offspring[fit_series.argmax()]) if best_current >= current_elite_fitness else elite[0]]
+        # elite = [deepcopy(offspring[fit_series.argmax()])]
+        # elite = [deepcopy(offspring[fit_series.argmin()])]
         # extra_returns.setdefault("elite", [])
         # extra_returns["elite"].append(list(elite[0]))
 
